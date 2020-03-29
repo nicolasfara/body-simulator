0205d5ea57b1bc4190cdaea57f89f559dfb86433
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-03-30 00:32:12 +0200

    Set java 8 as default

---
+++ src/main/java/it/unibo/pcd/presenter/worker/SimulatorMasterAgent.java
@@ -0,0 +1,12 @@
+package it.unibo.pcd.presenter.worker;
+
+public class SimulatorMaster extends Agent {
+    public SimulatorMaster(final String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
---
+++ src/main/java/it/unibo/pcd/presenter/worker/SimulatorWorkerAgent.java
@@ -0,0 +1,4 @@
+package it.unibo.pcd.presenter.worker;
+
+public class SimulatorWorkerAgent {
+}
