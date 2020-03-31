8df66b17f1ce93b2d8671e90f96c71aca33918f8
nic0lasfara <nicolas.farabegoli@gmail.com>
2020-03-31 19:28:54 +0200

    Create a resettable latch

--- src/main/java/it/unibo/pcd/presenter/worker/util/ResettableCountDownLatch.java
+++ src/main/java/it/unibo/pcd/presenter/worker/util/ResettableCountDownLatch.java
@@ -1,4 +1,28 @@
 package it.unibo.pcd.presenter.worker.util;
 
 public class ResettableCountDownLatch {
+    private final int nParticipants;
+    private int counter;
+
+    public ResettableCountDownLatch(final int nParticipants) {
+        this.nParticipants = nParticipants;
+        counter = nParticipants;
+    }
+    
+    public synchronized void await() throws InterruptedException {
+        while (counter > 0) {
+            wait();
+        }
+    }
+    
+    public synchronized void down() {
+        counter--;
+        if (counter == 0) {
+            notifyAll();
+        }
+    }
+    
+    public synchronized void reset() {
+        counter = nParticipants;
+    }
 }
