044927e6ad07b4a8c10e4f56e56a3c963e337ad3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-04-06 23:44:01 +0200

    Add logger

--- src/main/java/it/unibo/pcd/presenter/worker/Agent.java
+++ src/main/java/it/unibo/pcd/presenter/worker/Agent.java
@@ -1,14 +1,18 @@
 package it.unibo.pcd.presenter.worker;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public abstract class Agent extends Thread {
+    private final Logger logger = Logger.getLogger(Agent.class.getName());
 
     public Agent(final String name) {
         super(name);
     }
 
     public void log(final String message) {
-        synchronized (System.out) {
-            System.out.println("[" + getName() + "] " + message);
+        synchronized (logger) {
+            logger.log(Level.INFO,  "[" + getName() + "] " + message + "\n");
         }
     }
 }
